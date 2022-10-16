1facb57838b980dadf2ecd29f20aa22d4fbc7986
renatochristian <renatochristian@outlook.com>
2022-10-16 20:42:52 -0300

    adicionar comando ci storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,18 +1,25 @@
 module.exports = {
-	"stories": [
+	stories: [
 		"../src/**/*.stories.mdx",
 		"../src/**/*.stories.@(js|jsx|ts|tsx)",
 	],
-	"addons": [
+	addons: [
 		"@storybook/addon-links",
 		"@storybook/addon-essentials",
 		"@storybook/addon-interactions",
 	],
-	"framework": "@storybook/react",
-	"core": {
-		"builder": "@storybook/builder-vite",
+	framework: "@storybook/react",
+	core: {
+		builder: "@storybook/builder-vite",
 	},
-	"features": {
-		"storyStoreV7": true,
+	features: {
+		storyStoreV7: true,
+	},
+	staticDirs: ["../public"],
+	viteFinal: (config, { configType }) => {
+		if (configType === "PRODUCTION") {
+			config.base = "/design-system";
+		}
+		return config;
 	},
 };
