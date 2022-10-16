0f4384da96033ed8077f859053790fbe7aa43105
renatochristian <renatochristian@outlook.com>
2022-10-16 19:28:39 -0300

    adicionar comando ci storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 
 import "./App.css";
-import { Button } from "./components/Button";
-import { Text } from "./components/Text";
-import { TextInput } from "./components/TextInput";
-
 export function App() {
 	
 	return (
 		<div className="App">
-			<Text children="Lorem Ipsum" />
 		</div>
 	);
 }
